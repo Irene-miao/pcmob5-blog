bad69a55afaa46458d4ea89b9a14c0f1a5ed3241
Irene <irene_lee@live.com.sg>
2021-01-11 10:21:48 +0800

    Added getUsername function in AccountScreen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+
+const API = "http://irene2miao.pythonanywhere.com";
+const API_WHOAMI = "/whoami";
 
 export default function AccountScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+
+  async function getUsername() {
+    console.log("----Getting user name----");
+    const token = await AsyncStorage.getItem("token");
+    console.log(`Token is ${token}`);
+
+    try {
+      const response = await axios.get(API + API_WHOAMI, {
+        headers: {Authorization: `JWT ${token}`},
+      });
+      console.log("Got user name!");
+      console.log(response);
+    } catch (error) {
+      console.log("Error getting user name");
+      if (error.response) {
+        console.log(error.response.data);
+      } else {
+        console.log(error);
+      }
+    }
+  }
+
+  useEffect(() => {
+    getUsername();
+  }, []);
+
   function signOut() {
     AsyncStorage.removeItem("token");
     navigation.navigate("SignIn");
